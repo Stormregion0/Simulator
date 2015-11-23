fa7542b4f5dbaf4e7ce37d4e49e0bf93abe167b3
AlexanderKosch <alexander@alexander-linux>
2015-11-23 17:28:27 +0100

    test

---
+++ Simulator/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 23.11.2015
+ *      Author: alexander
+ */
+
+
+
+
+#include <iostream>                                     // Ein- und Ausgabebibliothek
+
+
+int main(){                                             // Hauptfunktion
+
+    std::cout << "Hallo, du schöne Welt!" << std::endl; // Ausgabe
+
+
+    return 0;                                           // Optionale Rückgabe an das Betriebssystem
+
+}
